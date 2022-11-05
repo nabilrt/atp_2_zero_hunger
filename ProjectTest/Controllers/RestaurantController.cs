3147a66935039b5742c610898e7a05b8a0201157
nabilrt <nabilrt51@gmail.com>
2022-11-05 17:49:14 +0600

    templating done with authentication forms

---
+++ ProjectTest/Controllers/RestaurantController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectTest.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
